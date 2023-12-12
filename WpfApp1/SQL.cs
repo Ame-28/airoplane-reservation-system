a20be6f675500377336ba0322f8de22cd1eba0c6
AbrahamVijai <116412034+AbrahamVijai@users.noreply.github.com>
2023-12-12 02:02:18 -0500

    Added code for SQL.cs

--- WpfApp1/SQL.cs
+++ WpfApp1/SQL.cs
@@ -1,12 +1,157 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
-namespace WpfApp1
+public class SQL
 {
-    internal class SQL
+    private readonly string connectionString;
+
+    public SQL(string server, string database, string userId, string password)
     {
+        connectionString = $"Server={server};Database={database};User ID={userId};Password={password};";
     }
+
+    public void insertValues(string tableName, Dictionary<string, object> values)
+    {
+        using (MySqlConnection connection = new MySqlConnection(connectionString))
+        {
+            try
+            {
+                connection.Open();
+                string columns = string.Join(",", values.Keys);
+                string parameters = string.Join(",", values.Keys.Select(key => $"@{key}"));
+                string query = $"INSERT INTO {tableName} ({columns}) VALUES ({parameters})";
+
+                using (MySqlCommand cmd = new MySqlCommand(query, connection))
+                {
+                    foreach (var kvp in values)
+                    {
+                        cmd.Parameters.AddWithValue($"@{kvp.Key}", kvp.Value);
+                    }
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error inserting record: {ex.Message}");
+            }
+        }
+    }
+
+    public List<Dictionary<string, object>> readValues(string tableName)
+    {
+        List<Dictionary<string, object>> records = new List<Dictionary<string, object>>();
+
+        using (MySqlConnection connection = new MySqlConnection(connectionString))
+        {
+            try
+            {
+                connection.Open();
+                string query = $"SELECT * FROM {tableName}";
+
+                using (MySqlCommand cmd = new MySqlCommand(query, connection))
+                {
+                    using (MySqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.HasRows)
+                        {
+                            while (reader.Read())
+                            {
+                                var record = new Dictionary<string, object>();
+                                for (int i = 0; i < reader.FieldCount; i++)
+                                {
+                                    string columnName = reader.GetName(i);
+                                    object value = reader.GetValue(i);
+                                    record[columnName] = value;
+                                }
+                                records.Add(record);
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("No rows found.");
+                        }
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error reading records: {ex.Message}");
+            }
+        }
+
+        return records;
+    }
+
+    public void alterValues(string tableName, Dictionary<string, object> newValues, string condition)
+    {
+        using (MySqlConnection connection = new MySqlConnection(connectionString))
+        {
+            try
+            {
+                connection.Open();
+
+                // Construct the SET part of the UPDATE query
+                string setClause = string.Join(",", newValues.Select(kvp => $"{kvp.Key} = @{kvp.Key}"));
+
+                // Construct the full UPDATE query
+                string query = $"UPDATE {tableName} SET {setClause} WHERE {condition}";
+
+                using (MySqlCommand cmd = new MySqlCommand(query, connection))
+                {
+                    // Add parameters for the new values
+                    foreach (var kvp in newValues)
+                    {
+                        cmd.Parameters.AddWithValue($"@{kvp.Key}", kvp.Value);
+                    }
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error updating records: {ex.Message}");
+            }
+        }
+    }
+
+    public bool checkValue(string tableName, string columnName, object valueToCheck)
+    {
+        bool valueExists = false;
+
+        using (MySqlConnection connection = new MySqlConnection(connectionString))
+        {
+            try
+            {
+                connection.Open();
+
+                // Construct the SELECT query to check the existence of the value
+                string query = $"SELECT COUNT(*) FROM {tableName} WHERE {columnName} = @ValueToCheck";
+
+                using (MySqlCommand cmd = new MySqlCommand(query, connection))
+                {
+                    // Add parameter for the value to check
+                    cmd.Parameters.AddWithValue("@ValueToCheck", valueToCheck);
+
+                    // ExecuteScalar returns the number of rows that satisfy the condition
+                    int rowCount = Convert.ToInt32(cmd.ExecuteScalar());
+
+                    // If rowCount is greater than 0, the value exists in the database
+                    valueExists = rowCount > 0;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error checking value: {ex.Message}");
+            }
+        }
+
+        return valueExists;
+    }
+
+
+
 }
