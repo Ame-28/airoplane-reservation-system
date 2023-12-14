b56e3bd25aab16eed39d017842420bf650383205
AbrahamVijai <116412034+AbrahamVijai@users.noreply.github.com>
2023-12-14 06:15:35 -0500

    Renamed 'WpfApp1' to 'ARS'

--- WpfApp1/Pages/Account.xaml.cs
+++ WpfApp1/Pages/Account.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WpfApp1.Pages
+namespace ARS.Pages
 {
     /// <summary>
     /// Interaction logic for Account.xaml
