cede7b57f45a8c54e61eeeec5afdef579e17e111
AbrahamVijai <116412034+AbrahamVijai@users.noreply.github.com>
2023-12-16 02:02:46 -0500

    Created Airport.cs

---
+++ WpfApp1/Airport.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ARS
+{
+    public static class Airport
+    {
+        public static string AirportName { get; }
+        public static string AirportCity { get; }
+        public static string IATACode { get;}        
+        public static int Terminals { get; }
+        public static int Gates { get; }
+    }
+}
--- WpfApp1/Pages/TravelDetails.xaml.cs
+++ WpfApp1/Pages/TravelDetails.xaml.cs
@@ -37,8 +37,12 @@ namespace ARS
         }
         private void SearchFlights_Click(object sender, RoutedEventArgs e)
         {
+            Dictionary<string, object> fromDetails = new Dictionary<string, object>();
+            Dictionary<string, object> toDetails = new Dictionary<string, object>();
+            SQL sql = new SQL;
+            // Get From and To details
 
         }
-        
+
     }
 }
