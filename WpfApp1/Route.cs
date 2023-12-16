aaed138e1b13b436f1a82a301332305ff314385c
AbrahamVijai <116412034+AbrahamVijai@users.noreply.github.com>
2023-12-16 10:45:26 -0500

    Create Route.cs

---
+++ WpfApp1/Route.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ARS
+{
+    public static class Route
+    {
+        public static string FromIATA { get; set; }
+        public static string FromCity { get; set; }
+        public static string ToIATA { get; set; }
+        public static string ToCity { get; set; }
+        public static int Distance { get; set; }
+        public static int Duration { get; set; }
+    }
+}
