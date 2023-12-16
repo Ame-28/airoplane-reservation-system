d3962cbc076a853ed8c45f5aae0aa66f73d47b6b
AbrahamVijai <116412034+AbrahamVijai@users.noreply.github.com>
2023-12-16 18:37:24 -0500

    Create Flight.cs

---
+++ WpfApp1/Flight.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ARS
+{
+    public class Flight
+    {
+        public string FlightName { get; set; }
+        public int FlightNo { get; set; }
+        public DateTime ArrivalTime { get; set; }
+        public DateTime DepartureTime { get; set; }
+    }
+}
