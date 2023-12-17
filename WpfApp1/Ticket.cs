73f338c9337964143da4c9ca0f63965ca0f2cea1
AbrahamVijai <116412034+AbrahamVijai@users.noreply.github.com>
2023-12-17 00:47:07 -0500

    Create Ticket.cs

---
+++ WpfApp1/Ticket.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ARS
+{
+    public class Ticket
+    {
+        // Departure details
+        public string FromCode { get; set; }
+        public string FromLocation { get; set; }
+        public string FromTime { get; set; }
+
+        // Arrival details
+        public string ToCode { get; set; }
+        public string ToLocation { get; set; }
+        public string ToTime { get; set; }
+
+        public string Duration { get; set; }
+
+        // Passenger details
+        public int PassengerCount { get; set; }
+
+        // Price of one ticket details
+        public decimal TicketPrice { get; set; }
+
+        // Grand Total Attributes
+        public decimal GrandTotal { get; set; }
+
+    }
+}
